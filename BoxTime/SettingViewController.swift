2e4c1bd61b23250e809202efd4da052c63fa1c06
Takeshi Kanazawa <oseans@hotmail.co.jp>
2020-04-10 22:06:30 +0900

    #2の作業完了

---
+++ BoxTime/SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  BoxTime
+//
+//  Created by 金澤武士 on 2020/04/10.
+//  Copyright © 2020 tk. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
